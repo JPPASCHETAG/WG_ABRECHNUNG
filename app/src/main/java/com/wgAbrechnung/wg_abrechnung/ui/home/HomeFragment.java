6a90a12b32951f48a5bf3a430351f406c76e24a4
Julian Paschetag <julian.paschetag@web.de>
2020-05-22 14:23:26 +0200

    Dialog hinzugefügt

--- app/src/main/java/com/wgAbrechnung/wg_abrechnung/ui/home/HomeFragment.java
+++ app/src/main/java/com/wgAbrechnung/wg_abrechnung/ui/home/HomeFragment.java
@@ -1,7 +1,9 @@
 package com.wgAbrechnung.wg_abrechnung.ui.home;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -9,6 +11,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -18,17 +22,20 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.wgAbrechnung.wg_abrechnung.CustomListAdapter;
-import com.wgAbrechnung.wg_abrechnung.FireMissilesDialogFragment;
 import com.wgAbrechnung.wg_abrechnung.HomeListAdapter;
 import com.wgAbrechnung.wg_abrechnung.R;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeFragment extends Fragment implements View.OnClickListener{
 
@@ -37,6 +44,8 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
 
     private ListView listView;
     private Button btnNewEntry;
+
+
 
     private ArrayList<String> ListZweck = new ArrayList<String>();
     private ArrayList<String> ListDatum = new ArrayList<String>();
@@ -112,8 +121,91 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
         switch (v.getId()) {
             case R.id.NEW_ENTRY:
                 System.out.println("Button gevlivkt");
-                FireMissilesDialogFragment dialog = new FireMissilesDialogFragment();
-                dialog.show(getFragmentManager(), "missiles");
+                // 1. Instantiate an <code><a href="/reference/android/app/AlertDialog.Builder.html">AlertDialog.Builder</a></code> with its constructor
+                final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+// 2. Chain together various setter methods to set the dialog characteristics
+                builder.setTitle("Einen Eintrag anlegen:");
+                        //.setMessage("Eine Nachricht");
+
+                LinearLayout layout = new LinearLayout(getActivity().getApplicationContext());
+                layout.setOrientation(LinearLayout.VERTICAL);
+
+                final EditText ZweckEditText = new EditText(getActivity().getApplicationContext());
+                ZweckEditText.setHint("Verwendungszweck");
+                layout.addView(ZweckEditText);
+
+                final EditText DateEditText = new EditText(getActivity().getApplicationContext());
+                DateEditText.setHint("Datum");
+                layout.addView(DateEditText);
+
+                final EditText NameEditText = new EditText(getActivity().getApplicationContext());
+
+                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+                String lastNameUsed = sharedPreferences.getString("LAST_USED_NAME", "noID");
+                if(lastNameUsed != "noID"){
+                    NameEditText.setText(lastNameUsed);
+                }else{
+                    NameEditText.setHint("Name");
+                }
+                layout.addView(NameEditText);
+
+                final EditText BetragEditText = new EditText(getActivity().getApplicationContext());
+                BetragEditText.setHint("Betrag");
+                layout.addView(BetragEditText);
+
+                builder.setView(layout);
+
+                builder.setPositiveButton("Hinzufügen", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        Map<String, Object> data = new HashMap<>();
+                        data.put("ZWECK", ZweckEditText.getText().toString());
+                        data.put("DATUM", DateEditText.getText().toString());
+                        data.put("NAME", NameEditText.getText().toString());
+                        data.put("BETRAG", BetragEditText.getText().toString());
+
+                        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("LAST_USED_NAME", NameEditText.getText().toString());
+                        editor.apply();
+
+
+                        db.collection(CURRENT_PROJEKT).document()
+                                .set(data)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+
+                                        Context context = getActivity().getApplicationContext();
+                                        CharSequence text = "Eintrag erfolgreich angelegt.";
+                                        Toast toast = Toast.makeText(context, text, Toast.LENGTH_LONG);
+                                        toast.show();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Context context = getActivity().getApplicationContext();
+                                        CharSequence text = "Fehler beim Anlegen des Eintrags.";
+                                        Toast toast = Toast.makeText(context, text, Toast.LENGTH_LONG);
+                                        toast.show();
+                                    }
+                                });
+                    }
+                });
+                builder.setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+
+// 3. Get the <code><a href="/reference/android/app/AlertDialog.html">AlertDialog</a></code> from <code><a href="/reference/android/app/AlertDialog.Builder.html#create()">create()</a></code>
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                //FireMissilesDialogFragment dialog = new FireMissilesDialogFragment();
+                //dialog.show(getFragmentManager(), "missiles");
                 break;
         }
     }
