5feba6907aadc363f5e3536eb9c5de4cde4274d9
Julian <Paschetag>
2020-06-01 10:42:44 +0200

    cleanup

--- app/src/main/java/com/wgAbrechnung/wg_abrechnung/HTTP_REQUEST.java
+++ app/src/main/java/com/wgAbrechnung/wg_abrechnung/HTTP_REQUEST.java
@@ -2,10 +2,6 @@ package com.wgAbrechnung.wg_abrechnung;
 
 import android.os.AsyncTask;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -35,11 +31,6 @@ public class HTTP_REQUEST extends AsyncTask<String, String, String> {
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
         delegate.processFinish(s);
-        /*try {
-            //JSONArray jsonArray = new JSONArray(s);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }*/
     }
 
     @Override
@@ -57,20 +48,6 @@ public class HTTP_REQUEST extends AsyncTask<String, String, String> {
         } catch (Exception e) {
             return null;
         }
-    }
-
-    private String[] Output(String json) throws JSONException {
-        JSONArray jsonArray = new JSONArray(json);
-        String[] stocks = new String[jsonArray.length()];
-        for (int i = 0; i < jsonArray.length(); i++) {
-            JSONObject obj = jsonArray.getJSONObject(i);
-            stocks[i] = obj.getString("VORNAME") + " " + obj.getString("NACHNAME");
-            System.out.println(obj);
-        }
-        System.out.println(jsonArray);
-        System.out.println(jsonArray.length());
-
-        return stocks;
     }
 
 
